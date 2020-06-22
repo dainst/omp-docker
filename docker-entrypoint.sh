c6440533dea8525a51eb5d0e37a66eb3cec690ec
Simon Hohl <simon.hohl@dainst.org>
2020-06-22 09:18:46 +0200

    Install dev deps and remove duplicate script part

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,8 +14,8 @@ if [ ! -f /var/www/html/config.inc.php ]; then
     npm run build
 
     # php modules
-    composer install -v -d lib/pkp --no-dev
-    composer install -v -d plugins/paymethod/paypal --no-dev
+    composer install -v -d lib/pkp
+    composer install -v -d plugins/paymethod/paypal
 
     # configuration
     cp config.TEMPLATE.inc.php config.inc.php
@@ -25,18 +25,11 @@ if [ ! -f /var/www/html/config.inc.php ]; then
     sed -i "s|config->set('Cache.SerializerPath', 'cache')|config->set('Cache.SerializerPath', '/tmp/cache')|" /var/www/html/lib/pkp/classes/core/PKPString.inc.php
     sed -i "s|return Core::getBaseDir() . DIRECTORY_SEPARATOR . 'cache';|return '/tmp/cache';|" /var/www/html/lib/pkp/classes/cache/CacheManager.inc.php # TODO: ammend repository and remove this sed? probably best to read an environment variable in PHP
 
-    chgrp -f -R www-data html/plugins
-    chmod -R 771 html/plugins
-    chmod g+s html/plugins
-    setfacl -Rm o::x,d:o::x html/plugins
-    setfacl -Rm g::rwx,d:g::rwx html/plugins
-
-
-    chgrp -f -R www-data html/public
-    chmod -R 771 html/public
-    chmod g+s html/public
-    setfacl -Rm o::x,d:o::x html/public
-    setfacl -Rm g::rwx,d:g::rwx html/public
+    chgrp -f -R www-data plugins
+    chmod -R 771 plugins
+    chmod g+s plugins
+    setfacl -Rm o::x,d:o::x plugins
+    setfacl -Rm g::rwx,d:g::rwx plugins
 
 fi
 
